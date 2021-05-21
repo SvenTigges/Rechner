60eda81829c92ff48c9bbc7acebdf80a95c78791
SvenTigges <sven.tigges@web.de>
2021-05-21 10:19:16 +0200

    Modul: Addition a + b

--- script_01.js
+++ script_01.js
@@ -5,6 +5,24 @@
 1. Dateneingabe + -überprüfung :: 
 2. Auswahl Rechenart :: 
 3. Fkt. Grundrechenarten :: 
-4. Ausgabe in Konsole :: 
+4. Ausgabe in Konsole :: check!
 */
+
+
+// Modul: Addition a + b | Test:
+ausgabe(adiieren(1,2)); //von innen nach ausen auflösen ->(1,1)= der Bezug -> (addieren(1,1)) = was soll gemacht werden-> wo soll es hin übertragen werden
+function adiieren(a,b) 
+{
+    let c = a + b;
+    return c;
+}
+
+
+
+// Modul Konsolenausgabe | Test: 
+// ausgabe("Hallo Welt");
+function ausgabe(outputStr) 
+{
+    console.log(outputStr);
+}
 
