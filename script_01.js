41c4b53adc3945768218b6779e018bd3e70aebcb
SvenTigges <sven.tigges@web.de>
2021-05-21 17:44:08 +0200

    Fertig

--- script_01.js
+++ script_01.js
@@ -8,28 +8,52 @@
 4. Ausgabe in Konsole :: check!
 */
 
-//ausgabe(rechner(getOp(),10,4));
-
-// Modul: operand eingabe | Test:
-ausgabe(getOp());
-function getOp() 
+//ausgabe(rechner(getOp(),getZahl(),getZahl()));
+start();
+function start() 
 {
-    let op = prompt("Bitte + | - | * | / eingeben.");
-   if (isOpValid(op)) 
-   {
-   return op ; 
-   } else
+    ausgabe(rechner(getOp(),getZahl("1"),getZahl("2")))    
+}
+
+// Modul: Zahl 1 eingeben | Test:
+//ausgabe(getZahl("1"));
+//ausgabe(getZahl("2"));
+function getZahl(numStr) 
+{
+    let zahl = parseInt(prompt("Bitte Zahl " + numStr + "eingeben:"));
+
+    while (isNaN(zahl)) 
     {
-      return "Bitte noch einmal!" 
-   }
+        zahl = parseInt(prompt("Das ist keine Zahl. Bitte erneut eingeben:"));
+    }    
+    return zahl; 
 }
 
 
-
-function isOpValid(checkStr) 
+// Modul: operand eingabe | Test:
+//ausgabe(getOp());
+function getOp() 
 {
-    // ....
-    return true;
+    let op = prompt("Bitte + | - | * | / eingeben.");
+    
+    while (!isOpValid(op)) {  // falsche eingabe // schleife = while 
+        op = prompt("Bitte einen korrekten Operator eingebe!")
+    }
+        return op; 
+}
+
+// Modul: Operand überprüfen | Test: 
+// ausgabe(isOpValid("+")); //<--Test
+// ausgabe(isOpValid("-"));
+// ausgabe(isOpValid("*"));
+// ausgabe(isOpValid("/"));
+// ausgabe(isOpValid("#"));
+// ausgabe(isOpValid(""));
+// ausgabe(isOpValid());
+function isOpValid(op) 
+{
+  
+    return (op == "+") || (op == "-") || (op == "*") || (op == "/");
 }
 
 // Modul: Rechenart auswählen | Test:
